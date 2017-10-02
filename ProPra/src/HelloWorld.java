3bb336aa9f6bcec8a437c64df864d038e88401d9
Daniel Brenner <Danielbrenner@gmx.net>
2017-10-02 22:09:04 +0200

    bla

--- ProPra/src/HelloWorld.java
+++ ProPra/src/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 		System.out.println("Hello World!");
 		System.out.println("Hallo? Jemand dahaaa?");
 		System.out.println("Also ehrlich...");
+		System.out.println("Jetzt aber!");
 	}
 
 }
