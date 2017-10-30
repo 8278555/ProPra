dcc581784d5672d6db64c5dfeba7d942a92b276e
Daniel Brenner <Danielbrenner@gmx.net>
2017-10-30 20:31:33 +0100

    Interfaces gebaut

--- ProPra/src/IArc.java
+++ ProPra/src/IArc.java
@@ -1,4 +1,11 @@
 
 public interface IArc {
-
+	public void SetID(String id);
+	String GetID();
+	public void SetName(String name);
+	String GetName();
+	public void SetPositionx(int x);
+	int GetPositionx();
+	public void SetPositiony(int y);
+	int GetPositiony();
 }
--- ProPra/src/ITransition.java
+++ ProPra/src/ITransition.java
@@ -1,4 +1,11 @@
 
 public interface ITransition {
-
+	public void SetID(String id);
+	String GetID();
+	public void SetName(String name);
+	String GetName();
+	public void SetPositionx(int x);
+	int GetPositionx();
+	public void SetPositiony(int y);
+	int GetPositiony();
 }
