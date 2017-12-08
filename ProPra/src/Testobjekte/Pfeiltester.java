fe65e70f4a925efa9fdc3b16ed7886c8ce6f3777
daniel.brenner <danielbrenner@gmx.net>
2017-12-08 12:20:56 +0100

    Pfeil tests

--- ProPra/src/Testobjekte/Pfeiltester.java
+++ ProPra/src/Testobjekte/Pfeiltester.java
@@ -9,7 +9,7 @@ public class Pfeiltester {
 	
 
 	public static void main(String[] args) {
-    	Pfeiltesterpanel testpanel = new Pfeiltesterpanel(new Point(150,50), new Point(250,100), 10.0d);
+    	Pfeiltesterpanel testpanel = new Pfeiltesterpanel();
 		Pfeiltesterframe testfenster = new Pfeiltesterframe("Fenstertext", testpanel);
 		testfenster.setVisible(true);
 	}
--- ProPra/src/Testobjekte/Pfeiltesterpanel.java
+++ ProPra/src/Testobjekte/Pfeiltesterpanel.java
@@ -1,65 +1,99 @@
 package Testobjekte;
 
-import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.LayoutManager;
-import java.awt.Point;
-import java.awt.Polygon;
 
 import javax.swing.JPanel;
 
 public class Pfeiltesterpanel extends JPanel {
 
-    private Math m;
-    private Point von, nach; // die zwei Punkte, die die Pfeil-Linie darstellen
-    private double phi;      // Rotationswinkel
-    private Polygon pfeil;
-    private double s;      // Seitenlaenge des Pfeils
-    private double xAlt, yAlt;          // x-,y-Werte vor der Rotation,relativ zur Pfeilspitze
-    private double xNeu, yNeu;          // x-,y-Werte nach der Rotation,absolut
-    private double xSpitze, ySpitze;    // x-,y-Werte der Pfeilspitze
-    public Pfeiltesterpanel(Point k1, Point k2, double length) {
-        von = k1;
-        nach = k2;
-        s = length;
-        phi = m.atan( ((double)nach.y  -  (double)von.y ) / ((double)nach.x  -  (double)von.x ) );
-        if(nach.x < von.x )
-            phi = phi + m.PI;
-        // den Pfeil zeichnen
-        pfeil = new Polygon();
-        pfeil.addPoint(nach.x , nach.y );  // Pfeilspitze
-        // Die beiden anderen Punkte des Pfeils muessen um den Winkel phi relative zur
-        // Pfeilspitze rotiert werden.
-        // x_rot = x*cos(phi) - y*sin(phi)
-        // y_rot = x*sin(phi) + y*cos(phi)
-        xSpitze = (double)nach.x;
-        ySpitze = (double)nach.y;
-        // erster Punkt
-        xAlt = (double)nach.x - s - xSpitze;
-        yAlt = (double)nach.y + s/2.5 - ySpitze;
-        xNeu = xAlt * m.cos(phi) - yAlt * m.sin(phi) + xSpitze;
-        yNeu = xAlt * m.sin(phi) + yAlt * m.cos(phi) + ySpitze;
-        pfeil.addPoint((int)xNeu, (int)yNeu);
-        // zweiter Punkt
-        yAlt = (double)nach.y - s/2.5 - ySpitze;
-        xNeu = xAlt * m.cos(phi) - yAlt * m.sin(phi) + xSpitze;
-        yNeu = xAlt * m.sin(phi) + yAlt * m.cos(phi) + ySpitze;
-        pfeil.addPoint((int)xNeu, (int)yNeu);
-       
-    }
-    
-    public void paintComponent(Graphics g){
-        Graphics2D g2d = (Graphics2D)g;
-       
-        g2d.setColor(Color.blue);
-  }
-    
-        public void zeichnen(Graphics2D g) {
-        g.drawLine(von.x, von.y, nach.x , nach.y );
-        g.fillPolygon(pfeil);
+	int x1;
+	int y1;
+	int x2;
+	int y2;
+	int x3;
+	int y3;
+	int x4;
+	int y4;
+	int x5;
+	int y5;
+	double m1;
+	double m2;
+	
+		
+	public Pfeiltesterpanel() {
+		x1 = 10;
+		y1 = 10;
+		x2 = 10;
+		y2 = 300;
+	}
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+        drawLine(g2);
     }
 
+	public void drawLine(Graphics2D g2) {
+		g2.drawLine(x1, y1, x2, y2);
+		calcTrianglePoints();
+//		g2.drawLine(x4, y4, x5, y5);
+		g2.drawLine(x3, y3, 490, 400);
+		//g2.drawArc(x2, y2, 100, 100, 45, 270);
+		
+		
+	}
+	public void calcTrianglePoints() {
+		m1 = (double)Math.abs(y1-y2);
+		m2 = (double)Math.abs(x1-x2);
+		double alphahelp = 90-(Math.toDegrees(Math.atan2((double)(y1-y2), (x1-x2)))%90);
+		double alpha = Math.sqrt(alphahelp*alphahelp);
+		double sinalpha;
+		if (Math.sin(alpha)<0) {
+			sinalpha = (Math.sin(alpha) * (-1));
+		}
+		else {sinalpha = Math.sin(alpha);}
+		double a = (sinalpha)*50;
+		double b = (Math.cos(alpha))*100;
+		double coeff = (m1 / m2);
+		
+//		double betahelp1 = Math.toDegrees(Math.atan2((double)(x1-x2), (y1-y2)))%90;
+//		double beta1 = Math.sqrt(betahelp1*betahelp1);
+//		double a1 = (Math.cos(beta1))*10;
+//		double b1 = (Math.cos(90-beta1)*10);
+		
+		System.out.println("Alpha: " + alpha);
+		System.out.println("Sin Alpha: " + sinalpha);
+		System.out.println("A: " + a);
+		System.out.println("B: " + b);
+		System.out.println("Y-Dist M1: " + m1);
+		System.out.println("X-Dist M2: " + m2);
+		System.out.println("Coeff: " + coeff);
+		
+		if (x1<x2) {
+			x3 = (int)Math.round((double)x2 - a);
+		}
+		else if (x1>x2) {
+			x3 = (int)Math.round((double)x2 + a);
+		}
+		else if (x1==x2) {
+			x3 = x2;
+		}
 
+		
+		if (y1<y2) {
+			y3 = (int)Math.round((double)x3 * coeff);
+		}
+		else if (y1>y2) {
+			y3 = (x3 * (int)Math.round(coeff));
+		}
+		else if (y1==y2) {
+			y3 = (x3 * (int)Math.round(coeff));
+		}
+		System.out.println("X3:" + x3);
+		System.out.println("Y3:" + y3);
 
+		
+	}
 }
