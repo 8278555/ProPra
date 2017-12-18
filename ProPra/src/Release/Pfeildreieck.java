3b6652eeae215648182344e8f5a47f711557eb9e
daniel.brenner <daniel.brenner@NBDEDZB-IT07.de.cplgroup.local>
2017-12-18 20:03:18 +0100

    Pfeilposition richtig gestellt

--- ProPra/src/Release/Pfeildreieck.java
+++ ProPra/src/Release/Pfeildreieck.java
@@ -10,10 +10,12 @@ public class Pfeildreieck {
 	int y2;
 	int x3;
 	int y3;
-	int x4;
-	int y4;
+    int x4;
+    int y4;
 	int x5;
 	int y5;
+	int x6;
+	int y6;
 	int factor;
 	double d;
 	double e;
@@ -23,6 +25,8 @@ public class Pfeildreieck {
 	double doubleY4;
 	double doubleX5;
 	double doubleY5;
+	double doubleX6;
+	double doubleY6;
 	
 	public Pfeildreieck(int x12, int y12, int x22, int y22, int elemsizefactor) {
 		this.x1 = x12;
@@ -34,8 +38,8 @@ public class Pfeildreieck {
 
 	public Polygon Pfeilspitze() {
 		calcTrianglePoints();
-		int xPoints[] = {x2, x4, x5};
-		int yPoints[]= {y2, y4, y5};
+		int xPoints[] = {x3, x5, x6};
+		int yPoints[]= {y3, y5, y6};
 		Polygon p = new Polygon(xPoints, yPoints, 3);
 		return p;
 	}
@@ -54,98 +58,121 @@ public class Pfeildreieck {
 		double a = (double)(y2-y1)/(double)(x2-x1);
 		double b = (double)y2-(a*(double)x2);
 		
+		if (Math.abs(x1-x2)<Math.abs(y1-y2)) {
+		    if (y1<y2) {
+		        doubleY3 = (double)y2-factor/2;
+		        doubleX3 = (doubleY3-b)/a;
+		    }
+		    else if (y1>y2) {
+                doubleY3 = (double)y2+factor/2;
+                doubleX3 = (doubleY3-b)/a;		        
+		    }
+		}
+		else if (Math.abs(x1-x2)>Math.abs(y1-y2)) {
+		    if (x1<x2) {
+		        doubleX3 = (double)x2-factor/2;
+		        doubleY3 = a*doubleX3 + b;
+		    }
+		    else if (x1>x2) {
+		        doubleX3 = (double)x2+factor/2;
+		        doubleY3 = a*doubleX3 + b;
+		    }
+		}
+		
 			if (x1<x2) {
 				//x2=x2-factor/2;
 				if (y1<y2) {
 					//y2=y2-factor/2;
-					doubleX3 = ((double)x2 - distB);
-					doubleY3 = a*doubleX3 + b;
-					doubleX4 = doubleX3 + distF;
-					doubleY4 = doubleY3 - (1/a*distF);
-					doubleX5 = doubleX3 - distF;
-					doubleY5 = doubleY3 + (1/a*distF);
+					doubleX4 = (doubleX3 - distB);
+					doubleY4 = a*doubleX4 + b;
+					doubleX5 = doubleX4 + distF;
+					doubleY5 = doubleY4 - (1/a*distF);
+					doubleX6 = doubleX4 - distF;
+					doubleY6 = doubleY4 + (1/a*distF);
 				}
 				else if (y1>y2) {
 					//y2=y2+factor/2;
-				    doubleX3 = ((double)x2 - distA);
-                    doubleY3 = a*doubleX3 + b;
-                    doubleX4 = doubleX3 + distE;
-                    doubleY4 = doubleY3 - (1/a*distE);
-                    doubleX5 = doubleX3 - distE;
-                    doubleY5 = doubleY3 + (1/a*distE);
+				    doubleX4 = (doubleX3 - distA);
+                    doubleY4 = a*doubleX4 + b;
+                    doubleX5 = doubleX4 + distE;
+                    doubleY5 = doubleY4 - (1/a*distE);
+                    doubleX6 = doubleX4 - distE;
+                    doubleY6 = doubleY4 + (1/a*distE);
                     }
 				else if (y1==y2) {
-					doubleX3 = ((double)x2 - factor);
-					doubleY3 = (double)y2;
-					doubleX4 = doubleX3;
-					doubleY4 = doubleY3 - (factor/2);
-					doubleX5 = doubleX3;
-					doubleY5 = doubleY3 + (factor/2);
+					doubleX4 = (doubleX3 - factor);
+					doubleY4 = doubleY3;
+					doubleX5 = doubleX4;
+					doubleY5 = doubleY4 - (factor/2);
+					doubleX6 = doubleX4;
+					doubleY6 = doubleY4 + (factor/2);
 				}
 			}
 			else if (x1>x2) {
 				//x2=x2+factor/2;
 				if (y1<y2) {
 					//y2=y2-factor/2;
-                    doubleX3 = ((double)x2 + distA);
-                    doubleY3 = a*doubleX3 + b;
-                    doubleX4 = doubleX3 + distE;
-                    doubleY4 = doubleY3 - (1/a*distE);
-                    doubleX5 = doubleX3 - distE;
-                    doubleY5 = doubleY3 + (1/a*distE);
+                    doubleX4 = (doubleX3 + distA);
+                    doubleY4 = a*doubleX4 + b;
+                    doubleX5 = doubleX4 + distE;
+                    doubleY5 = doubleY4 - (1/a*distE);
+                    doubleX6 = doubleX4 - distE;
+                    doubleY6 = doubleY4 + (1/a*distE);
 				}
 				else if (y1>y2) {
 					//y2=y2+factor/2;
-					doubleX3 = ((double)x2 + distB);
-					doubleY3 = a*doubleX3 + b;
-					doubleX4 = doubleX3 + distF;
-					doubleY4 = doubleY3 - (1/a*distF);
-					doubleX5 = doubleX3 - distF;
-					doubleY5 = doubleY3 + (1/a*distF);
+					doubleX4 = (doubleX3 + distB);
+					doubleY4 = a*doubleX4 + b;
+					doubleX5 = doubleX4 + distF;
+					doubleY5 = doubleY4 - (1/a*distF);
+					doubleX6 = doubleX4 - distF;
+					doubleY6 = doubleY4 + (1/a*distF);
 
 				}
 				else if (y1==y2) {
-					doubleX3 = ((double)x2 + factor);
-					doubleY3 = (double)y2;
-					doubleX4 = doubleX3;
-					doubleY4 = doubleY3 - (factor/2);
-					doubleX5 = doubleX3;
-					doubleY5 = doubleY3 + (factor/2);
+					doubleX4 = (doubleX3 + factor);
+					doubleY4 = doubleY3;
+					doubleX5 = doubleX4;
+					doubleY5 = doubleY4 - (factor/2);
+					doubleX6 = doubleX4;
+					doubleY6 = doubleY4 + (factor/2);
 				}
 			}
 			else if (x1==x2) {
 				if (y1<y2) {
-					doubleX3 = (double)x2;
-					doubleY3 = (double)y2 - factor;
-					doubleX4 = doubleX3 + factor/2;
-					doubleY4 = doubleY3;
-					doubleX5 = doubleX3 - factor/2;
-					doubleY5 = doubleY3;
+					doubleX4 = doubleX3;
+					doubleY4 = doubleY3 - factor;
+					doubleX5 = doubleX4 + factor/2;
+					doubleY5 = doubleY4;
+					doubleX6 = doubleX4 - factor/2;
+					doubleY6 = doubleY4;
 				}
 				else if (y1>y2) {
-					doubleX3 = (double)x2;
-					doubleY3 = (double)y2 + factor;
-					doubleX4 = doubleX3 + factor/2;
-					doubleY4 = doubleY3;
-					doubleX5 = doubleX3 - factor/2;
-					doubleY5 = doubleY3;
+					doubleX4 = doubleX3;
+					doubleY4 = doubleY3 + factor;
+					doubleX5 = doubleX4 + factor/2;
+					doubleY5 = doubleY4;
+					doubleX6 = doubleX4 - factor/2;
+					doubleY6 = doubleY4;
 
 				}
 				else if (y1==y2) {
-					doubleX3 = (double)x2;
-					doubleY3 = (double)y2;
 					doubleX4 = doubleX3;
 					doubleY4 = doubleY3;
-					doubleX5 = doubleX3;
-					doubleY5 = doubleY3;
+					doubleX5 = doubleX4;
+					doubleY5 = doubleY4;
+					doubleX6 = doubleX4;
+					doubleY6 = doubleY4;
 				}
 			}
-			x3 = (int)Math.round(doubleX3);
-			y3 = (int)Math.round(doubleY3);
-			x4 = (int)Math.round(doubleX4);
-			y4 = (int)Math.round(doubleY4);
+            x3 = (int)Math.round(doubleX3);
+            y3 = (int)Math.round(doubleY3);
+            x4 = (int)Math.round(doubleX4);
+            y4 = (int)Math.round(doubleY4);
 			x5 = (int)Math.round(doubleX5);
 			y5 = (int)Math.round(doubleY5);
+			x6 = (int)Math.round(doubleX6);
+			y6 = (int)Math.round(doubleY6);
 			}
 
 
